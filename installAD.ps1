3d08f8d1de2a5d37c9bac5bed44d767e72b2a0c0
simplilearnaz305 <98162105+simplilearnaz305@users.noreply.github.com>
2023-06-13 11:11:19 +0530

    Update installAD.ps1

--- installAD.ps1
+++ installAD.ps1
@@ -3,11 +3,11 @@ Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
 
 # Promote the server to a domain controller
 $adminCreds = Get-Credential -Message "Enter the credentials for a user with administrative privileges"
-Install-ADDSForest -DomainName "yourdomain.com" -SafeModeAdministratorPassword (ConvertTo-SecureString -String "P@ssw0rd" -AsPlainText -Force) -Credential $adminCreds -InstallDns -DomainMode Win2012R2 -ForestMode Win2012R2 -NoRebootOnCompletion
+Install-ADDSForest -DomainName "vinicloud.ga" -SafeModeAdministratorPassword (ConvertTo-SecureString -String "Passw0rd@356" -AsPlainText -Force) -Credential $adminCreds -InstallDns -DomainMode Win2012R2 -ForestMode Win2012R2 -NoRebootOnCompletion
 
 # Configure DNS settings
 $dnsServer = Get-DnsServerPrimaryZone
-Set-DnsServerPrimaryZone -Name "yourdomain.com" -DynamicUpdate Secure
+Set-DnsServerPrimaryZone -Name "vinicloud.ga" -DynamicUpdate Secure
 
 # Restart the server to complete the Active Directory installation
 Restart-Computer -Force
